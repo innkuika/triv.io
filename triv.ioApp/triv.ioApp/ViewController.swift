71e0fbdcf0d41dc68f9859c733436f9dc26880ad
Mamkh27 <mkheer@ucdavis.edu>
2021-02-13 18:01:18 -0800

    initial commit

---
+++ triv.ioApp/triv.ioApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  triv.ioApp
+//
+//  Created by Manprit Heer on 2/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
