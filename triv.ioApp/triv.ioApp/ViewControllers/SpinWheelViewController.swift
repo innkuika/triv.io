5001175f4355c81533149a708a5d568371297f2e
innkuika <innku.ika@gmail.com>
2021-02-23 12:45:55 -0800

    polished wheelUI

--- triv.ioApp/triv.ioApp/ViewControllers/SpinWheelViewController.swift
+++ triv.ioApp/triv.ioApp/ViewControllers/SpinWheelViewController.swift
@@ -16,7 +16,7 @@ class SpinWheelViewController: UIViewController {
     @IBOutlet weak var fortuneWheelViewOutlet: SwiftFortuneWheel!
     // pass in the category names here
     var SpinWheelTextArray: [String] = ["small KT", "middle KT", "big KT", "XL KT", "cute KT", "fat KT"]
-    var SpinWheelColorArray = [UIColor.red, UIColor.blue, UIColor.yellow, UIColor.purple, UIColor.cyan, UIColor.orange]
+    var SpinWheelColorArray = [trivioRed, trivioBlue, trivioYellow, trivioPurple, trivioGreen, trivioOrange]
     var finishIndex: Int?
     
     // query from database
@@ -52,35 +52,56 @@ class SpinWheelViewController: UIViewController {
     
     func renderWheel() {
         var slices: [Slice] = []
-        // slice customization
-        let textPreferences = TextPreferences(textColorType: SFWConfiguration.ColorType.evenOddColors(evenColor: .black, oddColor: .black), font: SFWFont.systemFont(ofSize: 15))
+        // wheel customization
         SpinWheelTextArray.forEach {
-            let textSliceContent = Slice.ContentType.text(text: $0, preferences: textPreferences)
+            let textSliceContent = Slice.ContentType.text(text: $0, preferences: textPreference())
             let slice = Slice(contents: [textSliceContent])
             slices.append(slice)
         }
-        fortuneWheelViewOutlet.pinImage = "long-arrow-up"
-        fortuneWheelViewOutlet.isSpinEnabled = true
-
-        // wheel customizations
-//        let sliceColorType = SFWConfiguration.ColorType.evenOddColors(evenColor: .red, oddColor: .cyan)
-        let pin = SFWConfiguration.PinImageViewPreferences(size: CGSize(width: 13, height: 40), position: .top, verticalOffset: -25)
-        let spin = SFWConfiguration.SpinButtonPreferences(size: CGSize(width: 50, height: 20))
-        let colorArray = SpinWheelColorArray
-        let sliceColorType = SFWConfiguration.ColorType.customPatternColors(colors: colorArray, defaultColor: SFWColor.brown)
-        let slicePreferences = SFWConfiguration.SlicePreferences(backgroundColorType: sliceColorType, strokeWidth: 1, strokeColor: .black)
-        let circlePreferences = SFWConfiguration.CirclePreferences(strokeWidth: 10, strokeColor: .black)
-        let wheelPreferences = SFWConfiguration.WheelPreferences(circlePreferences: circlePreferences, slicePreferences: slicePreferences, startPosition: .bottom)
-        let configuration = SFWConfiguration(wheelPreferences: wheelPreferences, pinPreferences: pin, spinButtonPreferences: spin)
-        
-        fortuneWheelViewOutlet.configuration = configuration
+        fortuneWheelViewOutlet.spinImage = "redCenterImage"
+        fortuneWheelViewOutlet.pinImage = "redArrow"
+        fortuneWheelViewOutlet.isSpinEnabled = false
+        fortuneWheelViewOutlet.pinImageViewCollisionEffect = CollisionEffect(force: 15, angle: 30)
+        fortuneWheelViewOutlet.edgeCollisionDetectionOn = true
         fortuneWheelViewOutlet.slices = slices
+
+        
+        fortuneWheelViewOutlet.configuration = wheelConfiguration()
     }
     
     let scoreArcStartAngleArray = [180, 216, 252, 288, 324]
     func renderScoreBoard(userScore: [String], botScore: [String]){
         renderScore(playerScore: userScore, isUser: true)
         renderScore(playerScore: botScore, isUser: false)
+    }
+    
+    func textPreference() -> TextPreferences{
+        let font =  UIFont.systemFont(ofSize: 23, weight: .heavy)
+        var textPreferences = TextPreferences(textColorType: SFWConfiguration.ColorType.customPatternColors(colors: nil, defaultColor: .white),font: font,verticalOffset: 5)
+        
+        textPreferences.orientation = .vertical
+        textPreferences.alignment = .right
+        return textPreferences
+    }
+    
+    func wheelConfiguration () -> SFWConfiguration{
+        // wheel customizations
+        let pin = SFWConfiguration.PinImageViewPreferences(size: CGSize(width: 13, height: 40), position: .top, verticalOffset: -25)
+        let spin = SFWConfiguration.SpinButtonPreferences(size: CGSize(width: 20, height: 20))
+        let colorArray = SpinWheelColorArray
+        let sliceColorType = SFWConfiguration.ColorType.customPatternColors(colors: colorArray, defaultColor: SFWColor.white)
+        let slicePreferences = SFWConfiguration.SlicePreferences(backgroundColorType: sliceColorType, strokeWidth: 0, strokeColor: .black)
+        let circlePreferences = SFWConfiguration.CirclePreferences(strokeWidth: 25, strokeColor: UIColor(red: 28/255, green: 71/255, blue: 126/255, alpha: 1.0))
+        var wheelPreferences = SFWConfiguration.WheelPreferences(circlePreferences: circlePreferences, slicePreferences: slicePreferences, startPosition: .bottom)
+        
+
+        let centerAnchorImage = SFWConfiguration.AnchorImage(imageName: "blueAnchorImage", size: CGSize(width: 15, height: 15), verticalOffset: -6)
+        wheelPreferences.centerImageAnchor = centerAnchorImage
+//        let anchorImage = SFWConfiguration.AnchorImage(imageName: "circleGradient", size: CGSize(width: 50, height: 50))
+//        wheelPreferences.imageAnchor = anchorImage
+        let configuration = SFWConfiguration(wheelPreferences: wheelPreferences, pinPreferences: pin, spinButtonPreferences: spin)
+        
+        return configuration
     }
     
     
@@ -103,8 +124,6 @@ class SpinWheelViewController: UIViewController {
                 renderArc(startAngleRadius: CGFloat($0.1), radiusPercent: radiusPercent, color: UIColor.gray.cgColor)
             }
         }
-        
-        
     }
     func renderArc(startAngleRadius: CGFloat, radiusPercent: Float, color: CGColor) {
         guard let parent = scoreBoardUIViewOutlet else { return }
---
+++ triv.ioApp/utility.swift
@@ -0,0 +1,17 @@
+//
+//  utility.swift
+//  triv.ioApp
+//
+//  Created by Jessica Wu on 2/23/21.
+//
+
+import Foundation
+import UIKit
+import SwiftFortuneWheel
+
+let trivioRed = UIColor(red: 233/255, green: 70/255, blue: 60/255, alpha: 1.0)
+let trivioOrange = UIColor(red: 239/255, green: 135/255, blue: 57/255, alpha: 1.0)
+let trivioYellow = UIColor(red: 240/255, green: 192/255, blue: 66/255, alpha: 1.0)
+let trivioGreen = UIColor(red: 117/255, green: 252/255, blue: 151/255, alpha: 1.0)
+let trivioBlue = UIColor(red: 37/255, green: 90/255, blue: 246/255, alpha: 1.0)
+let trivioPurple = UIColor(red: 92/255, green: 61/255, blue: 245/255, alpha: 1.0)
