3842f07215259e678a6dd5ee9d2840dd2681f3d9
innkuika <innku.ika@gmail.com>
2021-03-03 23:07:04 -0800

    query profile info from database and display

--- triv.ioApp/triv.ioApp/ViewControllers/UserPreferenceViewController.swift
+++ triv.ioApp/triv.ioApp/ViewControllers/UserPreferenceViewController.swift
@@ -13,16 +13,69 @@ import Firebase
 import FBSDKLoginKit
 
 class UserPreferenceViewController: UIViewController{
+    @IBOutlet weak var avatarImageViewOutlet: UIImageView!
+    @IBOutlet weak var userNameLabelOutlet: UILabel!
+    @IBOutlet weak var uidButtonOutlet: UIButton!
+    
+    @IBOutlet weak var coinNumberLabelOutlet: UILabel!
+    
+    let workerGroup = DispatchGroup()
+    
+    // query from database
+    var ref: DatabaseReference!
+    var avatarNumber = 1
+    var userName = "guest"
+    var userId = ""
+    var coinNumber = 0
     
     @IBOutlet weak var logoutButtonOutlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        renderUI()
+        ref = Database.database().reference()
+        workerGroup.enter()
+        
+        // get latest data from database
+        getUserProfileData()
+        
+        // do following once finish querying data from database
+        workerGroup.notify(queue: DispatchQueue.main) {
+            self.renderUI()
+        }
     }
     
     func renderUI(){
         styleButton(button: logoutButtonOutlet)
+        print("user id: \(userId)")
+        uidButtonOutlet.setTitle(userId, for: .normal)
+        avatarImageViewOutlet.image = UIImage(named: "Robot Avatars_1.png")
+        userNameLabelOutlet.text = userName
 
+        coinNumberLabelOutlet.text = "\(coinNumber)"
+    }
+    @IBAction func uidButtonPressed(_ sender: Any) {
+    }
+    
+    func getUserProfileData(){
+        guard let user = Auth.auth().currentUser else {
+            assertionFailure("Unable to get current logged in user")
+            return
+        }
+        
+        self.userId = user.uid
+        
+        self.ref.child("User/\(user.uid)").observe(DataEventType.value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            
+            // get user profile info
+            let unwrappedAvatarNumber = value?["AvatarNumber"] as? Int ?? 1
+            guard let unwrappedUserName = value?["Name"] as? String else { return }
+            guard let unwrappedCoinNumber = value?["AvatarNumber"] as? Int else { return }
+            
+            self.userName = unwrappedUserName
+            self.avatarNumber = unwrappedAvatarNumber
+            self.coinNumber = unwrappedCoinNumber
+        })
+        workerGroup.leave()
     }
     
     @IBAction func logoutButtonPressed(_ sender: Any) {
